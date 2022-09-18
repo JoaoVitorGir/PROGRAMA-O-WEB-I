bda6fe7b367cbad365e33eb3ecd78e1d9cafe355
JoaoVitorGir <joao.girardi@unidavi.edu.br>
2022-09-18 19:36:16 -0300

    Create index.php

---
+++ Atividades_15_09/Cadastro de filmes/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>".$_GET['codigo']."</h1>";
+        echo "<h1>".$_GET['nome']."</h1>";
+        echo "<h1>".$_GET['sobrenome']."</h1>";
+    ?>
+</body>
+</html>
