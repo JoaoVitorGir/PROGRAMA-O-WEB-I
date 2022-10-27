e2b2cff331e1a48cb6f8d455a537c53a42ffd0ce
JoaoVitorGir <joao.girardi@unidavi.edu.br>
2022-10-26 21:31:06 -0300

    Atualização do e-commerce

--- E-Commerce/index.php
+++ E-Commerce/index.php
@@ -63,33 +63,9 @@
         <div class="row">
             
         </div>
-        <div class="row">
-            <div class="col-2">
-                <?php
-                    $sql = "SELECT * FROM meu_commerce.categorias WHERE categoria_pai is null";
-                    $consulta = $conn->prepare($sql);
-                    $consulta->execute();
-
-                    foreach($consulta as $linha){
-                ?>
-                    <a href="?categoria=<?php echo $linha['id'];?>">
-                        <div class="item-menu"><?php echo $linha['descricao'];?></div>
-                    </a>
-                    <?php
-                        //listar as sub-categorias
-                        $sql_itens = "SELECT * FROM meu_commerce.categorias WHERE categoria_pai = ".$linha['id'];
-                        $subitens = $conn->prepare($sql_itens);
-                        $subitens->execute();
-                        foreach($subitens as $item){
-                    ?>
-                    - <a href="?categoria=<?php echo $item['id'];?>"><?php echo $item['descricao'];?></a><br>
-                    <?php
-                        }
-                    }
-                ?>
-            </div>
-
-            <div class="col-10">
+        <div class="row" >
+          <!-- Inicio lista de Imgens -->
+        <div class="col-8">
                 <?php
                     if(isset($_GET['categoria'])){
                         $sql = "SELECT p.id as id_produto, 
@@ -119,8 +95,8 @@
                    
                     foreach($consulta as $linha){?>
 
-                    <div class="card" style="width: 18rem; display: inline-block; margin: 10px;">
-                        <img style="width: 18rem;" src="<?php echo $linha['imagem'];?>" alt="...">
+                    <div class="card" style="width: 14rem; display: inline-block; margin: 5px;">
+                        <img style="width: 14rem;" src="<?php echo $linha['imagem'];?>" alt="...">
                         <div class="card-body">
                             <a class="desc" href="descricao.php"><h5 class="card-title"><?php echo $linha['descricao'];?></h5></a>
                             <p class="card-text"><?php echo $linha['resumo']?></p>
@@ -130,7 +106,61 @@
                     <?php
                     }
                 ?>
-            </div>
+            </div>  <!-- Final da lista de imagens -->
+
+            <!-- Lista de produtos Incio -->
+            <div class="col-3" style="margin-left: 90px;">
+              
+              <!-- lista Inicio -->
+              
+                <ol class="list-group list-group-numbered">
+                    <?php
+                        $sql = "SELECT * FROM meu_commerce.categorias WHERE categoria_pai is null";
+                        $consulta = $conn->prepare($sql);
+                        $consulta->execute();
+                       //for que vai listar as categorias principais 
+                        foreach($consulta as $linha){
+                    ?>
+
+
+
+
+                                <li class="list-group-item d-flex justify-content-between align-items-start" style="border-bottom: none;">
+                                    <div class="ms-2 me-auto">
+                                      <div class="fw-bold">
+                                        <a style="color:black; text-decoration:none; hover-back" 
+                                                
+                                                 onMouseOver="this.style.color='#87cefa'" onMouseOut="this.style.color='#111'" href="?categoria=<?php echo $linha['id'];?>">
+
+                                          <!-- aqui passa o valor da categoria principal " AQUI " -->
+                                          <div class="item-menu"><?php echo $linha['descricao'];?></div>
+                                        </a>
+                                      </div>
+                                      <?php
+                                      //listar as sub-categorias
+                                      $sql_itens = "SELECT * FROM meu_commerce.categorias WHERE categoria_pai = ".$linha['id'];
+                                      $subitens = $conn->prepare($sql_itens);
+                                      $subitens->execute();
+                                      foreach($subitens as $item){
+                                     ?>
+                                                  <!-- aqui passa os sub produtos da categoria principal " AQUI "-->
+                                               <a style="color:grey ; text-decoration:none" href="?categoria=<?php echo $item['id'];?>"><?php echo $item['descricao'];?></a><br>
+                                              <?php
+                                              }
+                          }
+                    ?>   
+                                    </div>
+                                </li>
+              
+                </ol>
+
+                
+              <!-- lista Final -->
+                          
+            </div> <!-- final lista de produtos -->
+
+
+            
         </div>
         <div class="row" style="background-color: blue;">
             Rodapé
@@ -142,3 +172,19 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<!-- nova lista 
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>  
+  </div>
+nova lista -->
